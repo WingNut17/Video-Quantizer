2cb0c61b25904c34612b6b0730282a9c5125cb6d
WingNut17 <shawn.sinclair56@gmail.com>
2024-04-27 18:51:43 -0400

    Update video_quantizer.py

--- video_quantizer.py
+++ video_quantizer.py
@@ -13,37 +13,37 @@ def extract_and_quantize_frames(input_file, output_folder):
     print("Quantizing frames...")
     
     # Make tiny palette Image, one black pixel
-    palIm = Image.new('P', (1,1))
+    paletteImage = Image.new('P', (1,1))
 
-    # Make your desired B&W palette containing only 1 pure white and 255 pure black entries
+    # Make B&W palette containing only 1 pure white and 255 pure black entries
     palette = [255, 255, 255 ] + [0, 0 ,0] * 255 
 
-    # Push in our lovely B&W palette and save just for debug purposes
-    palIm.putpalette(palette)
-    palIm.save(f"{output_folder}\\palette.png")
+    # Push in B&W palette and save for debug purposes
+    paletteImage.putpalette(palette)
+    paletteImage.save(f"{output_folder}\\palette.png")
 
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
-    # Load the video clip
+    # Load the video
     clip = VideoFileClip(input_file)
     
-    # Iterate over each frame in the video clip
+    # Iterate over each frame in the video
     for i, frame in enumerate(clip.iter_frames(fps=clip.fps)):
         # Convert the NumPy array to a PIL Image
         pil_frame = Image.fromarray(frame)
 
         # Quantize the frame to the specified palette
-        quantized_frame = pil_frame.quantize(palette=palIm, method=0)
+        quantized_frame = pil_frame.quantize(palette=paletteImage, method=0)
         
-        # Save the quantized frame as a PNG image
+        # Save the quantized frames as PNG's image
         frame_path = os.path.join(output_folder, f"frame_{i:04d}.png")
         quantized_frame.save(frame_path)
     
     print("Frames Quantized.")
     
-    # Close the video clip
+    # Close the video
     clip.close()
 
     images_to_mp4(output_folder, f"{output_folder}\\output.mp4", fps=30)
@@ -53,7 +53,6 @@ def images_to_mp4(input_folder, output_file, fps=30):
     image_files = [file for file in os.listdir(input_folder) if file.endswith((".png", ".jpg", ".jpeg")) and file != "palette.png"]
     image_files.sort()  # Sort the files to maintain order
     
-    # Initialize a list to hold the frames
     frames = []
     
     # Load each image file and append it to the frames list
@@ -62,12 +61,11 @@ def images_to_mp4(input_folder, output_file, fps=30):
         frames.append(image_path)
 
     
-    # Create an ImageSequenceClip from the frames list
+    # Create a video from the frames
     video_clip = ImageSequenceClip(frames, fps=fps)
-    
-    # Write the video clip to an MP4 file
     video_clip.write_videofile(output_file, codec="libx264", fps=fps)
 
+    # Deletes the frames, keeps the video and palette
     for image_file in image_files:
         os.remove(os.path.join(input_folder, image_file))
     
